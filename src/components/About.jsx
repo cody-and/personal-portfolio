ce435992e41ff4021c5160ac0576ba47fbb67bc2
cody-and <codya.0807@icloud.com>
2023-09-02 19:06:55 -0500

    finsihed about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,26 @@
-export default function About() {
+import React from 'react';
 
-
-
-
+const About = () => {
   return (
-    <>
-    <h3>About Me</h3>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus, eveniet. Assumenda voluptatum veniam ipsum sapiente mollitia! Itaque quasi provident perspiciatis nam recusandae voluptatibus incidunt ullam facilis, tempore labore dolores aliquam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate cumque placeat ipsa vero iure ab labore consectetur. Nam repellendus voluptatum eos. Delectus reprehenderit rerum deserunt necessitatibus consequatur assumenda voluptates vero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae distinctio velit dolorem dolor exercitationem debitis fuga suscipit magnam, delectus ut at, placeat assumenda nam nihil harum est, magni obcaecati eum.</p>
-    </>
-  )
-}
+    <div className="about-container">
+      <h3>About Me</h3>
+      <p>
+        Welcome to my personal portfolio! I'm Cody Anderson, a Twin Cities-based Web Developer and Business Student with a keen interest in driving innovation and building businesses. My educational journey after high school commenced at Northwood Technical College (formerly known as WITC) when I enrolled in the Business Management program.
+      </p>
+
+      <p>
+        During my tenure at Northwood Tech, I discovered a deep-seated passion for working with computers and web development. This newfound enthusiasm prompted me to take a significant step forward in my academic journey by enrolling in the Full-Time Full Stack Coding Bootcamp offered by the University of Minnesota.
+      </p>
+
+      <p>
+        In the coding bootcamp, I immersed myself in the world of web development, diving into front-end and back-end technologies, databases, and application development. This experience solidified my commitment to pursuing a career in web development, blending my business acumen with the technical skills required to innovate and contribute to the digital landscape.
+      </p>
+
+      <p>
+        I invite you to explore my portfolio to see how my journey has shaped my skills and projects, reflecting my dedication to both business and web development.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- src/components/EducationComponent.jsx
+++ src/components/EducationComponent.jsx
@@ -12,7 +12,7 @@ export default function EducationComponent() {
           <div className="card-text">
             <h2>Northwood Tech</h2>
             <h4>Business Management</h4>
-            <p>Some description or details about Northwood University.</p>
+            <p>During my enrollment in the Business Management program, I honed my skills for potential employment across a wide array of business sectors or for launching my own venture. My career prospects encompassed hospitality, sales, technology, retail, manufacturing, and financial services. Throughout my tenure at Northwood Tech, I mastered the art of devising and executing business strategies. Furthermore, I had the flexibility to specialize in either general business or marketing. The program also afforded me invaluable hands-on experience through field study opportunities in authentic business environments.</p>
           </div>
         </div>
         <div className="education-card">
@@ -20,7 +20,7 @@ export default function EducationComponent() {
           <div className="card-text">
             <h2>University of Minnesota</h2>
             <h4>Full Stack Coding Bootcamp</h4>
-            <p>Some description or details about University of Minnesota.</p>
+            <p>In the University of Minnesota Full Stack Coding Bootcamp, I gained comprehensive web development skills. The program covered front-end and back-end technologies, databases, and application development, focusing on JavaScript, HTML, and CSS. With experienced instructors and real-world projects, I honed my coding abilities, teamwork, and problem-solving skills. The program's emphasis on industry-relevant tools prepared me for a successful career in web development, supported by a portfolio of practical projects.</p>
           </div>
         </div>
       </div>
