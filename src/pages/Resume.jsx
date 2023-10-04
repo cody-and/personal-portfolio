4d6231ef03cdfb78a1b53d60781f9dcf336754e6
cody-and <codya.0807@icloud.com>
2023-10-04 10:45:33 -0500

    changing resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MyPDF from '../assets/Resume/Resume.pdf';
+import MyPDF from '../assets/Resume/UpdatedResume.pdf';
 import '../App.css'
 
 export default function Resume() {
